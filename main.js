aa970d4b74d7f130a9e8b395752ffbcc53e5bc14
Ceptive <66751646+Ceptive@users.noreply.github.com>
2021-05-19 14:37:53 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-
+//========================================CONSTANTS ON READY========================================
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const { botInfo, prefix, token }
+//==============================================LOGIN===============================================
+client.on('ready', () => {
+  console.log("Online")
+})
